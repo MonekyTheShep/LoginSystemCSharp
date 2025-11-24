cad17a16d8b85f2996e9e669706babd3a14ce0dc
Moneky <237399958+MonekyTheShep@users.noreply.github.com>
2025-11-24 22:22:05 +0000

    Code

--- loginSystem/MongoDB.cs
+++ loginSystem/MongoDB.cs
@@ -1,24 +1,31 @@
-using Microsoft.VisualBasic;
 using MongoDB.Driver;
 namespace loginSystem
 {
     public class HandleMongoDB : IDatabaseHandling
     {
-        string password;
+        string client;
+        string database;
+        string collection;
               
         /// <summary>connect to a mongodb database</summary>
+        /// <param name="client">The database client</param>
+        /// <param name="database">The database you want to look through.</param>
+        /// <param name="collection">The collections in the database you want to look through</param>
 
-        public HandleMongoDB(string password)
+        public HandleMongoDB(string client = null, string database = null, string collection = null)
         {
-            this.password = password;
+            this.client = client;
+            this.database = database;
+            this.collection = collection;
         }
         public void connect() 
         {
-            
+            var _client = new MongoClient(client);  
+            var _database = _client.GetDatabase(database);
+            var _collection = _database.GetCollection<Task>(collection);
         }
-        public void disconnect()
-        {
-            
-        }
+
+        // no disconnect function in mongodb
+        public void disconnect(){}
     }    
 }
